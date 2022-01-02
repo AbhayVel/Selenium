883d0a92611aa167aad686c55fbe95b0ab9ad67c
sajagkarki <sajagkarki@gmail.com>
2022-01-02 10:43:38 -0600

    Added Login

--- FirstKeywordDrivenFramework/FirstKeywordDrivenFramework/Program.cs
+++ FirstKeywordDrivenFramework/FirstKeywordDrivenFramework/Program.cs
@@ -9,8 +9,15 @@ namespace FirstKeywordDrivenFramework
     {
         static void Main(string[] args)
         {
-             
-             
+            MyDriver aDriver = new MyDriver();
+            aDriver.Driver.Manage().Window.Maximize();
+            aDriver.Driver.Navigate().GoToUrl("https://opensource-demo.orangehrmlive.com/");
+
+            LoginPage loginPage = new LoginPage(aDriver.Driver);
+
+            loginPage.SetUserName("Admin");
+            loginPage.SetPassword("admin123");
+            loginPage.Submit();
         }
     }
 }
---
+++ FirstKeywordDrivenFramework/FirstKeywordDrivenFramework/ReadPropertyFile.cs
@@ -0,0 +1,53 @@
+﻿using NPOI.Util.Collections;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace FirstKeywordDrivenFramework
+{
+	
+	public class ReadPropertyFile
+    {
+		Properties p = new Properties();
+		public void OpenProperty(string path)
+        {
+			
+			// This example will give you idea about File handling with properties 
+			FileStream fs = null;
+			try
+			{
+				fs = new FileStream(path, FileMode.Open, FileAccess.ReadWrite);
+			}
+			catch (FileNotFoundException e1)
+			{
+				// TODO Auto-generated catch block
+				Console.WriteLine(e1.ToString());
+				Console.Write(e1.StackTrace);
+			}
+
+
+			
+			try
+			{
+				System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+
+				p.Load(fs);
+				Console.WriteLine("hi");
+			}
+			catch (IOException e)
+			{
+				// TODO Auto-generated catch block
+				Console.WriteLine(e.ToString());
+				Console.Write(e.StackTrace);
+			}
+		}
+
+		public string GetValue(string key)
+        {
+
+			return p[key].ToString(); 
+
+        }
+    }
+}
